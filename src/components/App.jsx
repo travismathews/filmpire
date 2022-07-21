b100e122f4e350b0ee3684c3dca71b18a4b96e40
Travis Mathews <travis@travismathews.io>
2022-07-20 22:20:18 -0400

    moving to laptop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <h1>Hello, World - Filmpire</h1>
+  </div>
 );
 
 export default App;
