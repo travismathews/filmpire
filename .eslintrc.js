516ecee79eb50c557caf2e29be492553f623d70b
Travis Mathews <travis@travismathews.io>
2022-07-24 16:44:20 -0400

    Finished scaffolding initial components and structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: [
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieInformation = () => {
+  console.log('Movie information');
+
+  return (
+    <div>MovieInformation</div>
+  );
+};
+
+export default MovieInformation;
