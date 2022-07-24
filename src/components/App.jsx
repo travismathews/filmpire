d42bde57d157e81c20768f387741625949c7ebfc
Travis Mathews <travis@travismathews.io>
2022-07-24 17:03:31 -0400

    Connected base styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,27 +4,34 @@ import { Route, Switch } from 'react-router-dom';
 
 import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <Switch>
-        <Route exact path="/movie/:id">
-          <MovieInformation />
-        </Route>
-        <Route exact path="/actors/:id">
-          <Actors />
-        </Route>
-        <Route exact path="/">
-          <Movies />
-        </Route>
-        <Route exact path="/profile/:id">
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
