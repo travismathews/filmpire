5857bb1caf989403cc818d3e9e134d2ead55349c
Travis Mathews <travis@travismathews.io>
2022-08-01 21:21:09 -0400

    Added tooltip to movie rating to show precise rating

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -7,6 +7,7 @@ export default makeStyles((theme) => ({
   links: {
     alignItems: 'center',
     fontWeight: 'bolder',
+    textDecoration: 'none',
     [theme.breakpoints.up('xs')]: {
       display: 'flex',
       flexDirection: 'column',
@@ -14,7 +15,7 @@ export default makeStyles((theme) => ({
     },
     '&:hover': {
       cursor: 'pointer',
-      textDecoration: 'none',
+
     },
   },
   image: {
